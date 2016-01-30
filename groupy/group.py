c4ed564e83091eea8085a4349dc369b09166a11d
Scott Hellman <scott@scutellaris.com>
2016-01-30 15:50:21 -0700

    override ==, len

--- groupy/group.py
+++ groupy/group.py
@@ -20,21 +20,28 @@ class Group(object):
 
     def __init__(self, operator_dict):
         self.operator_dict = operator_dict
+        self.elements = {name:self.Element(self,name) for name in operator_dict.keys()}
 
     def get_element(self, name):
-        return self.Element(self, name)
+        return self.elements[name]
 
     def multiply(self, left, right):
-        return self.Element(self, self.operator_dict[left][right])
+        return self.elements[self.operator_dict[left][right]]
 
     def __getitem__(self, index):
-        return self.Element(self, index)
+        return self.elements[index]
 
+    def __len__(self):
+        return self.compute_order()
+
+    def compute_order(self):
+        return len(self.elements)
 
     class Element(object):
         def __init__(self, group, name):
             self.name = name
             self.group = group
+            #self.order = self.compute_order()
 
         def __mul__(self, other):
             return self.group.multiply(self.name, other.name)
@@ -42,8 +49,25 @@ class Group(object):
         def __rmul__(self, other):
             return self.group.multiply(other.name, self.name)
 
+        def __eq__(self, other):
+            return self.name == other.name and self.group == other.group
+
+        def __ne__(self, other):
+            return not self.__eq__(other)
+
         def __repr__(self):
             return "{} in [{}]".format(self.name, hash(self.group))
 
         def __str__(self):
             return str(self.name)
+
+        def __len__(self):
+            return self.compute_order()
+
+        def compute_order(self):
+            current = self*self
+            count = 1
+            while current != self:
+                current*=self
+                count += 1
+            return count
--- tests/test_group.py
+++ tests/test_group.py
@@ -1,10 +1,27 @@
 from groupy import group
+import pytest
 
 def test_construct_from_matrix():
     matrix = [[0,1,2],
               [1,2,0],
               [2,0,1]]
-    G = group.Group.from_matrix(matrix)
+    G = group.from_matrix(matrix)
     for i,row in enumerate(matrix):
         for j,element in enumerate(row):
             assert matrix[i][j] == G.operator_dict[i][j]
+
+def test_group_access():
+    matrix = [[0,1,2],
+              [1,2,0],
+              [2,0,1]]
+    G = group.from_matrix(matrix)
+    assert G[0].name == 0
+    assert G[1].name == 1
+    assert G[2].name == 2
+
+@pytest.mark.parametrize("n",[1,4,7])
+def test_construct_cyclic(n):
+    C = group.cyclic(n)
+    for i in xrange(n):
+        for j in xrange(n):
+            (C[i] * C[j]).name == (i+j)%n
